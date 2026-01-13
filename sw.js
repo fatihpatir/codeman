b6d8002cb94915d0e3ef0974949e054214d3ed2f
fatihpatir <fatihpatir.hoca@gmail.com>
2026-01-13 20:40:34 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,24 @@
-const CACHE_NAME = 'parkur-v1';
+const CACHE_NAME = 'parkur-v2';
 const ASSETS = [
     './',
     './index.html',
     './style.css',
     './game.js',
-    './manifest.json'
+    './manifest.json',
+    './motherboard_bg.png',
+    './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => cache.addAll(ASSETS))
     );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
